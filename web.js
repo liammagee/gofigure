632545a1cdf0f69fd1134c8e130159074f362de0
doclm <liam.magee@gmail.com>
2012-07-28 19:04:05 +1000

    fix to package

--- web.js
+++ web.js
@@ -3,12 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.configure(function() {
-	app.use(express.static(__dirname + '/test'));
+	app.use(express.static(__dirname ));
 });
 
-app.get('/', function(request, response) {
-	response.send('Hello World');
-});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
