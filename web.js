bb327c5727519470d40d6cf20029418f44d15cfd
doclm <liam.magee@gmail.com>
2012-07-28 19:07:04 +1000

    fix to package

--- web.js
+++ web.js
@@ -3,6 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.configure(function() {
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(app.router);
 	app.use(express.static(__dirname ));
 });
 
